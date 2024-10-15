e21092d8050862b6d1402481fc111aa4dfdc5217
Nafisa Axmed <axmednafisa@gmail.com>
2024-10-15 10:30:43 +0100

    Fetch and display recipes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState("");
+  const [recipeData, setRecipeData] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/recipes?select=name")
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data.recipes);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedRecipe) {
+      fetch(`https://dummyjson.com/recipes/${selectedRecipe}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setRecipeData(data);
+        });
+    }
+  }, [selectedRecipe]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select
+        value={selectedRecipe}
+        onChange={(e) => {
+          setSelectedRecipe(e.target.value);
+        }}
+      >
+        <option value="" defaultValue>
+          Recipes
+        </option>
+        {recipes.map((recipe) => (
+          <option key={recipe.id} value={recipe.id}>
+            {recipe.name}
+          </option>
+        ))}
+      </select>
+
+      {recipeData.length !== 0 ? (
+        <div>
+          <div>
+            <h3>{recipeData.name}</h3>
+            <p>
+              Prep: {recipeData.prepTimeMinutes} mins Cook:{" "}
+              {recipeData.cookTimeMinutes} mins {recipeData.difficulty}
+            </p>
+            <p>
+              Serves: {recipeData.servings} Calories per serving:{" "}
+              {recipeData.caloriesPerServing}
+            </p>
+            <img
+              src={recipeData.image}
+              style={{ height: 300, width: 300 }}
+              alt="recipe"
+            />
+          </div>
+          <div>
+            <div>
+              <h3>Ingredients</h3>
+              {recipeData.ingredients.map((ingredient) => (
+                <p>{ingredient}</p>
+              ))}
+            </div>
+            <div>
+              <h3>Method</h3>
+              {recipeData.instructions.map((instruction, index) => (
+                <p>
+                  {index + 1}. {instruction}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 export default App;
