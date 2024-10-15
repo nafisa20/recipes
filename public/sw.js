39d2c42c4813e3fb2670bbe7a7b54610322322b9
Nafisa Axmed <axmednafisa@gmail.com>
2024-10-15 10:51:22 +0100

    Cache already fetched data

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,28 @@
-const CACHE_NAME = "my-cache";
+/* eslint-disable no-restricted-globals */
 
-Window.self.addEventListener("install", (e) => {
+const CACHE_NAME = "my-cache";
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["./", "/.App.js"]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
       return cache
-        .addAll(["./", "/.App.js"])
-        .then(() => Window.self.skipWaiting());
+        .match(event.request, { ignoreVary: true })
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
     })
   );
 });
